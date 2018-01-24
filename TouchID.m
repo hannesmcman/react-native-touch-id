c190d794bbd73a07e0c9aa3b432ff51395f0aed5
Johannes Carlsen <hannes.mcman@gmail.com>
2018-01-23 23:47:11 -0600

    Fix biometryType issue and remove examples

--- TouchID.m
+++ TouchID.m
@@ -9,7 +9,7 @@ RCT_EXPORT_METHOD(isSupported: (RCTResponseSenderBlock)callback)
 {
     LAContext *context = [[LAContext alloc] init];
     NSError *error;
-    
+
     if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
         callback(@[[NSNull null], [self getBiometryType:context]]);
         // Device does not support TouchID
@@ -24,7 +24,7 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
 {
     LAContext *context = [[LAContext alloc] init];
     NSError *error;
-    
+
     // Device has TouchID
     if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
         // Attempt Authentification
@@ -35,50 +35,50 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
              // Failed Authentication
              if (error) {
                  NSString *errorReason;
-                 
+
                  switch (error.code) {
                      case LAErrorAuthenticationFailed:
                          errorReason = @"LAErrorAuthenticationFailed";
                          break;
-                         
+
                      case LAErrorUserCancel:
                          errorReason = @"LAErrorUserCancel";
                          break;
-                         
+
                      case LAErrorUserFallback:
                          errorReason = @"LAErrorUserFallback";
                          break;
-                         
+
                      case LAErrorSystemCancel:
                          errorReason = @"LAErrorSystemCancel";
                          break;
-                         
+
                      case LAErrorPasscodeNotSet:
                          errorReason = @"LAErrorPasscodeNotSet";
                          break;
-                         
+
                      case LAErrorTouchIDNotAvailable:
                          errorReason = @"LAErrorTouchIDNotAvailable";
                          break;
-                         
+
                      case LAErrorTouchIDNotEnrolled:
                          errorReason = @"LAErrorTouchIDNotEnrolled";
                          break;
-                         
+
                      default:
                          errorReason = @"RCTTouchIDUnknownError";
                          break;
                  }
-                 
+
                  NSLog(@"Authentication failed: %@", errorReason);
                  callback(@[RCTMakeError(errorReason, nil, nil)]);
                  return;
              }
-             
+
              // Authenticated Successfully
              callback(@[[NSNull null], @"Authenticat with Touch ID."]);
          }];
-        
+
         // Device does not support TouchID
     } else {
         callback(@[RCTMakeError(@"RCTTouchIDNotSupported", nil, nil)]);
@@ -89,11 +89,10 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
 - (NSString *)getBiometryType:(LAContext *)context
 {
     if (@available(iOS 11, *)) {
-        return context.biometryType == LABiometryTypeFaceID ? @"FaceID" : @"TouchID";
+        return (context.biometryType == LABiometryTypeFaceID) ? @"FaceID" : @"TouchID";
     }
-    
+
     return @"TouchID";
 }
 
 @end
-
